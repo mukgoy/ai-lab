4dc49c76fafe0dd9582957231758a905ffda4220
Mukesh Goyal <m.goyal1988@gmail.com>
2022-01-04 00:35:50 +0530

    add relational id in response

--- backend/src/admin/entities/faq.entity.ts
+++ backend/src/admin/entities/faq.entity.ts
@@ -1,5 +1,5 @@
 import { UserEntity } from 'src/auth/entities/user.entity';
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, RelationId} from 'typeorm';
 import { BotEntity } from './bot.entity';
 import { CommonProperty } from './common.property';
 
@@ -16,6 +16,9 @@ export class FaqEntity extends CommonProperty{
 
     @ManyToOne(() => BotEntity, bots => bots.faqs)
     bot: BotEntity;
+
+    @RelationId((faq: FaqEntity) => faq.bot) // you need to specify target relation
+    botId: number;
 
     @ManyToOne(() => UserEntity, owner => owner.faqs)
     owner: UserEntity;
