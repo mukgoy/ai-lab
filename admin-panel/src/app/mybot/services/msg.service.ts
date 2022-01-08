7e3e343cc06c6d33f83427de8261289520c2ce87
Mukesh Goyal <m.goyal1988@gmail.com>
2022-01-08 10:31:41 +0530

    bot redesign

--- admin-panel/src/app/mybot/services/msg.service.ts
+++ admin-panel/src/app/mybot/services/msg.service.ts
@@ -6,15 +6,20 @@ import { NlpService } from './nlp.service';
 })
 export class MsgService {
 
+  isBotReplying: boolean = false;
   msgs: any[] = [];
   constructor(public nlpService:NlpService){
 
   }
   onUserReply(msg:string){
     this.msgs.push({type:"human", msg:msg, time:new Date().getTime()})
+    this.isBotReplying = true;
     this.nlpService.process(msg).then((res:string)=>{
-      res = res.replace(/<p>(.*?)<\/p>/, '$1');
-      this.msgs.push({type:"bot", msg:res, time:new Date().getTime()});
+      setTimeout(()=>{
+        this.isBotReplying = false;
+        res = res.replace(/<p>(.*?)<\/p>/, '$1');
+        this.msgs.push({type:"bot", msg:res, time:new Date().getTime()});
+      },1500)
     })
   }
 }
