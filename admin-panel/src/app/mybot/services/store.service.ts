d9ae3802506a05fd2591b155bd2be59290e30cd6
Mukesh Goyal <m.goyal1988@gmail.com>
2022-02-01 15:21:12 +0530

    Update store.service.ts

--- admin-panel/src/app/mybot/services/store.service.ts
+++ admin-panel/src/app/mybot/services/store.service.ts
@@ -7,6 +7,7 @@ import { BotEntity, ChatUserEntity } from 'src/app/shared/entities';
 })
 export class StoreService {
   ls = localStorage;
+  isAgent = false
 
   bots: BotEntity[] = [];
   onlineUsers: ChatUserEntity[] = []
@@ -16,15 +17,27 @@ export class StoreService {
 
   public botUserSubject: BehaviorSubject<ChatUserEntity>;
   constructor() {
+    //use case for ChatUserType = agent
+    const currentUser = this.ls.getItem('currentUser');
+    let agentUser = currentUser !== null ? JSON.parse(currentUser) : null;
+    if(agentUser){
+      this.isAgent = true
+    }
+
     const userJson = this.ls.getItem('botUser');
     let user = userJson !== null ? JSON.parse(userJson) : {};
     this.botUserSubject = new BehaviorSubject<ChatUserEntity>(user);
   }
   set botUser(user: ChatUserEntity) {
-    this.ls.setItem('botUser', JSON.stringify(user));
+    if(!this.isAgent){
+      this.ls.setItem('botUser', JSON.stringify(user))
+    };
     this.botUserSubject.next(user);
   }
   get botUser() {
+    if(this.isAgent){ 
+      return {} as ChatUserEntity
+    }
     return this.botUserSubject.value;
   }
 
