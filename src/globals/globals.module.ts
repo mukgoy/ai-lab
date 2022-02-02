21827e3e6bbca0a56d88a76ab82fc442b69b5c0c
Mukesh Goyal <m.goyal1988@gmail.com>
2022-02-02 17:17:19 +0530

    password reset

--- src/globals/globals.module.ts
+++ src/globals/globals.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailModule } from './modules/mail/mail.module';
 import repositories from './repository';
 @Global()
 @Module({
     imports: [
         TypeOrmModule.forRoot(),
         TypeOrmModule.forFeature([...repositories]),
+        MailModule,
     ],
     providers: [],
     exports: [TypeOrmModule],
