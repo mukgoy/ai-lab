e5caaaa79bf6ade011e66a33fb76a7f37e807cfb
Mukesh Goyal <mgoyal@enchoice.com>
2023-08-27 23:48:26 +0530

    new deployment

--- admin-panel/src/environments/environment.prod.ts
+++ admin-panel/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  uploads: "https://ai-lab-backend.herokuapp.com/",
-  backend: "https://ai-lab-backend.herokuapp.com/",
+  uploads: "https://lalabot-api.onrender.com/",
+  backend: "https://lalabot-api.onrender.com/",
   frontend: "https://ai-lab-frontend.herokuapp.com/",
   bundleJs : "https://ai-lab-frontend.herokuapp.com/assets/mybot/js/bundle.js",
 };
